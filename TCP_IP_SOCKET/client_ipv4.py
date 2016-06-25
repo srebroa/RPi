001ba27a5551a17890ee695ef5f8a81484e275b2
srebroa <srebroa@gmail.com>
2016-06-25 19:04:48 +0200

    Socket ipv4 client server example update

--- TCP_IP_SOCKET/client_ipv4.py
+++ TCP_IP_SOCKET/client_ipv4.py
@@ -1,11 +1,15 @@
 # Echo client program for IPv4 only
 import socket
 
-HOST = '192.168.0.14'    # The remote host (RPi IP)
+HOST = '192.168.0.11'    # The remote host (RPi IP)
 PORT = 55005             # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send('7778888')
+print 'Socket created'
+try:
+   s.connect((HOST, PORT))
+   s.send('7778888')
+except socket.error as msg:
+   print 'Error code: ' + str(msg[0]) + 'Msg ' + str(msg[1])
 data = s.recv(1024)
 s.close()
 print 'Received test number:', repr(data)
--- TCP_IP_SOCKET/server_ipv4.py
+++ TCP_IP_SOCKET/server_ipv4.py
@@ -4,14 +4,19 @@ import socket
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 55005              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+print 'Socket created'
+try:
+   s.bind((HOST, PORT))
+except socket.error as msg:
+   print 'Error code'
 s.listen(1)
 conn, addr = s.accept()
 print 'Connected by', addr
 while 1:
     data = conn.recv(1024)
     if not data: break
-    conn.send(data)
+    print 'I got the following data:', repr(data)
+    conn.send(data+"test")
 conn.close()
 
 
